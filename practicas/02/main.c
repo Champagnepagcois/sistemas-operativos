8c31197f4f331a9ef666211716876c7d79e6ac85
CHAMPA <kalilon@Kalilon.local>
2023-10-19 01:12:41 -0600

    Ci: refactor code

--- practicas/02/main.c
+++ practicas/02/main.c
@@ -62,12 +62,12 @@ void freeMemoryShared(int clave){
 
 /****************Imprimir matriz completa*********************/
 void ImprimirMatrizCompleta(int *matriz, int columnas, int filas){
-  for(int i=0;i<filas;i++){
-    for(int j=0;j<columnas;j++){
-      printf("\t%d",matriz[j+i]);
+  for(int i=0; i< columnas*filas;i++){
+    if(i%columnas == 0){
+      printf("\n");
     };
-    printf("\n");
-  };
+    printf("\t%d",matriz[i]);
+  }
 }
 /**************************************************************/
 
@@ -106,12 +106,32 @@ int sumaFila( int *matriz, int tam_columna,int num_fila){
 
 /****************************************************************************/
 
+/****************Region critica lectura*********************/
+void leerRegionCritica(int *apt_mat_a, int num_col, int fila,int hijo){
+  printf("\n----ENTRA A REGION CRITICA DE LECTURA---\n");
+  printf("Hijo %d: La fila a operar es:\t", hijo);
+  imprimirFila(apt_mat_a,num_col,fila);
+  printf("\n----SALE DE REGION CRITICA DE LECTURA---\n\n");
+};
+
+/***********************************************************/
+
+/***************Region critica escritura********************/
+void escribirRegionCritica(int *apt_mat_a,int *apt_mat_b, int num_col, int fila,int hijo){
+  printf("\n----ENTRA A REGION CRITICA DE ESCRITURA---\n");
+  printf("\nHijo %d: El resultado de la suma es:%d\n", hijo ,sumaFila(apt_mat_a,NUM_COLUMNAS,fila));
+  *apt_mat_b= sumaFila(apt_mat_a,NUM_COLUMNAS,fila);
+  printf("\n----SALE DE REGION CRITICA DE ESCRITURA---\n");
+};
+/**********************************************************/
 
 int main(){
-  int semafoto_memoria;
-  key_t llave;
-  llave = ftok("archivo",'m');
-  semafoto_memoria = Crea_semaforo(llave,1);
+  int semafoto_memoria_leer,semaforo_memoria_escribir;
+  key_t llave_acceder, llave_ingresar;
+  llave_acceder = ftok("archivo",'m');
+  llave_ingresar = ftok("archivo3",'k');
+  semafoto_memoria_leer = Crea_semaforo(llave_acceder,1);
+  semaforo_memoria_escribir = Crea_semaforo(llave_ingresar,1);
 
   int matriz [] = {
     1,2,3,4,5,6,7,8,9,
@@ -144,15 +164,13 @@ int main(){
   if(pid1 ==-1) crearError();
   if(pid1==0){
     presentarse(1);
-    //Accedemos a la MC
-    down(semafoto_memoria);
-    printf("Hijo 1: La fila a operar es:\t");
-    imprimirFila(apt_mat_a,NUM_COLUMNAS,0);
-    printf("\nHijo 1: El resultado de la suma es:%d\n", sumaFila(apt_mat_a,NUM_COLUMNAS,0));
-    apt_mat_b[0]= sumaFila(apt_mat_a,NUM_COLUMNAS,0);
+    down(semafoto_memoria_leer); //Accedemos a region critica lectura
+    leerRegionCritica(apt_mat_a,NUM_COLUMNAS,0,1);
+    up(semafoto_memoria_leer); //Salimos de region critica lectura
+    down(semaforo_memoria_escribir); //Accedemos a region critica escritura
+    escribirRegionCritica(apt_mat_a,&apt_mat_b[0],NUM_COLUMNAS,0,1);
+    up(semaforo_memoria_escribir); //Salimos de region critica escritura
     //sleep(2);
-    //Dejamos la MC
-    up(semafoto_memoria);
     exit(0);
   };
 
@@ -160,15 +178,13 @@ int main(){
   if(pid2 ==-1) crearError();
   if(pid2==0){
     presentarse(2);
-    //Accedemos a la MC
-    down(semafoto_memoria);
-    printf("Hijo 2:La fila a operar es:\t");
-    imprimirFila(apt_mat_a,NUM_COLUMNAS,1);
-    printf("\nHijo 2: El resultado de la suma es:%d\n", sumaFila(apt_mat_a,NUM_COLUMNAS,1));
-    apt_mat_b[1]= sumaFila(apt_mat_a,NUM_COLUMNAS,1);
-    //sleep(3);
-    //Dejamos la MC
-    up(semafoto_memoria);
+    down(semafoto_memoria_leer); //Accedemos a region critica lectura
+    leerRegionCritica(apt_mat_a,NUM_COLUMNAS,1,2);
+    up(semafoto_memoria_leer); //Salimos de region critica lectura
+    down(semaforo_memoria_escribir); //Accedemos a region critica escritura
+    escribirRegionCritica(apt_mat_a,&apt_mat_b[1],NUM_COLUMNAS,1,2);
+    up(semaforo_memoria_escribir); //Salimos de region critica escritura
+    //sleep(2);
     exit(0);
   };
 
@@ -176,15 +192,13 @@ int main(){
   if(pid3 ==-1) crearError();
   if(pid3==0){
     presentarse(3);
-    //Accedemos a la MC
-    down(semafoto_memoria);
-    printf("Hijo 3: La fila a operar es:\t");
-    imprimirFila(apt_mat_a,NUM_COLUMNAS,2);
-    printf("\nHijo 3: El resultado de la suma es:%d\n", sumaFila(apt_mat_a,NUM_COLUMNAS,2));
-    apt_mat_b[2]= sumaFila(apt_mat_a,NUM_COLUMNAS,2);
-    //sleep(4);
-    //Dejamos la MC
-    up(semafoto_memoria);
+    down(semafoto_memoria_leer); //Accedemos a region critica lectura
+    leerRegionCritica(apt_mat_a,NUM_COLUMNAS,2,3);
+    up(semafoto_memoria_leer); //Salimos de region critica lectura
+    down(semaforo_memoria_escribir); //Accedemos a region critica escritura
+    escribirRegionCritica(apt_mat_a,&apt_mat_b[2],NUM_COLUMNAS,2,3);
+    up(semaforo_memoria_escribir); //Salimos de region critica escritura
+    //sleep(2);
     exit(0);
   };
 
@@ -195,13 +209,15 @@ int main(){
     wait(NULL);
   };
 
+  printf("\n\n**************Los procesos hijos han terminado*********************\n\n");
+
   //Imprime la matriz de resultados
-  printf("\n\nLos resultados de la suma, mostrados en el proceso padre son:\n");
+  printf("\n\nLos resultados de la suma guardados en la region de memoria compartida, mostrados desde el proceso padre son:\n");
   ImprimirMatrizCompleta(apt_mat_b,1,3);
 
   freeMemoryShared(CLAVE_MAT_A);
   freeMemoryShared(CLAVE_MAT_B);
 
-  //sleep(5);
+  //sleep(4);
   return 0;
 }
