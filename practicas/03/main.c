070122e221e688ec4cec0d3e718c93ff315cde19
CHAMPA <kalilon@Kalilon.local>
2023-10-24 12:42:45 -0600

    ci: refactor code

--- practicas/03/main.c
+++ practicas/03/main.c
@@ -6,7 +6,16 @@
 #define NUM_COLUMNAS 9
 #define NUM_FILAS 3
 
+/****Definimos la estructura que mandaremos como parametro a la funcion****/
+typedef struct Parametro { 
+  int* filaMAtriz; //Definir la direccion de memoria donde empieza la fila de la matriz
+  int* resultadoMAtriz; //Definir la direccion de memoria del resultado
+  int tam_col;
+  int num_hilo;
+  pthread_t id_hilo;
+};
 
+/****************************************************************************/
 /***************manda mensaje de error y termina los procesos**************/
 void crearError(){
   perror("Ocurrio un error al crear el hilo");
@@ -16,83 +25,63 @@ void crearError(){
 
 /*******************************Operacion hilo*******************************/
 void *operacionHilo(void *arg){
-  int *numeros = (int*)arg;
-  int resultado = 1;
-  printf("\nLa fila a operar es:\n");
-  for(int i=0; i<NUM_COLUMNAS;i++){
-    printf("\t%d",numeros[i]);
-    resultado *= numeros[i];
-  };
-  printf(" =  %d\n",resultado);
-}
-/****************************************************************************/
-
-void *test(void *arg){
   struct Parametro *param  =(struct Parametro*)arg;
-  int resultado = param->resultadoMAtriz;
+  *(param->resultadoMAtriz) =1;
+  printf("\nHola soy el hilo %d y mi identificador es: %d\n", param->num_hilo, param->id_hilo);
+  printf("\nHilo %d:\tLa fila que voy a operar es:\n",param->num_hilo);
+  printf("Hilo %d:",param->num_hilo);
   for(int i=0; i<param->tam_col;i++){
     printf("\t%d",param->filaMAtriz[i]);
-    resultado *= param->filaMAtriz[i];
+    *(param->resultadoMAtriz) *= param->filaMAtriz[i];
   };
-  printf(" =  %d\n",resultado);
-
-  
-}
-/****Definimos la estructura que mandaremos como parametro a la funcion****/
-typedef struct Parametro{ 
-  int* filaMAtriz; //Definir la direccion de memoria donde empieza la fila de la matriz
-  int* resultadoMAtriz;
-  int tam_col; 
+  printf("\nHilo %d: El resultado de multiplicar toda la fila es: %d\n",param->num_hilo,*(param->resultadoMAtriz));
 };
-
 /****************************************************************************/
+
 int main(){
-  pthread_t id_hilo1,id_hilo2,id_hilo3;
+  pthread_t id_hilo[3];
 
   int matriz [] = {
     1,2,3,4,5,6,7,8,9,
     1,3,5,7,9,11,13,15,17,
     2,4,6,8,10,12,14,16,18
   };
+  
   int resultadosMatriz [3];
   int hiloCorrecto[3];
 
   struct Parametro param[3];
-  param[0].filaMAtriz = &matriz[0];
-  param[0].resultadoMAtriz = &resultadosMatriz[0];
-  param[0].tam_col = NUM_COLUMNAS;  
+  //Inicializamos las estructuras
+  for(int i=0;i<NUM_FILAS;i++){
+  int posicion = i==0?0:i==1?9:18;
+  param[i].filaMAtriz = &matriz[posicion];
+  param[i].resultadoMAtriz = &resultadosMatriz[i];
+  param[i].tam_col = NUM_COLUMNAS;  
+  param[i].num_hilo =i+1;
+  param[i].id_hilo =id_hilo[i];
+  };
 
-  param[1].filaMAtriz = &matriz[9];
-  param[1].resultadoMAtriz = &resultadosMatriz[1];
-  param[1].tam_col = NUM_COLUMNAS;  
-
-  param[2].filaMAtriz = &matriz[18];
-  param[2].resultadoMAtriz = &resultadosMatriz[2];
-  param[2].tam_col = NUM_COLUMNAS;  
 
   //Creamos hilos
   printf("\n\n*****************Creamos hilos*****************\n\n");
 
-  hiloCorrecto[0] = pthread_create(&id_hilo1,NULL, operacionHilo,(void*) test);
-  if(hiloCorrecto[0]!= 0) crearError();
-  printf("El identificador del hilo 1 es : %d\n",id_hilo1);
-
-  hiloCorrecto[1] = pthread_create(&id_hilo2,NULL, operacionHilo,(void*)&(matriz[9]));
-  if(hiloCorrecto[1]!= 0) crearError();
-  printf("El identificador del hilo 2 es : %d\n",id_hilo2);
-  
-  hiloCorrecto[2] = pthread_create(&id_hilo3,NULL, operacionHilo,(void*)&(matriz[18]));
-  if(hiloCorrecto[2]!= 0) crearError();
-
-  printf("El identificador del hilo 3 es : %d\n",id_hilo3);
+  for(int i=0;i<NUM_FILAS;i++){
+    hiloCorrecto[i] = pthread_create(&(id_hilo[i]),NULL, operacionHilo,(void*) &(param[i]));
+    param[i].id_hilo = id_hilo[i];
+    if(hiloCorrecto[i]!= 0) crearError();
+  };
 
   //Esperamos a que los hilos acaben
-
-  pthread_join(id_hilo1,NULL);
-  pthread_join(id_hilo2,NULL);
-  pthread_join(id_hilo3,NULL);
-
+  for(int i =0;i<NUM_FILAS;i++){
+    pthread_join(id_hilo[i],NULL);
+  };
   printf("\n\n**************Los hilos han terminado*********************\n\n");
+
+  //Mostramos resultados desde el hilo padre
+  printf("\n\n**************Proceso padre muestra los resultados*********************\n\n");
+  for(int i=0;i<NUM_FILAS;i++){
+    printf("Resultados de la operacion en la fila %d es : %d\n",i+1,resultadosMatriz[i]);
+  };
 
   return 0;
 }
