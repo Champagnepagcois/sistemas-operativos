a26802c7c18ba78473db9c3dad10034d859fd8c2
CHAMPA <kalilon@Kalilon.local>
2023-10-28 04:14:34 -0600

    ci:build data structure Producto

--- proyecto/Model.h
+++ proyecto/Model.h
@@ -1,16 +1,26 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include <stdlib.h>
+#include <string.h>
+
+//Se encarga ded todo lo que deberia se DB
+//CRUD, conexion a DB, etc
+
 struct Usuario {
   int ID_usuario;
   char nombre [15];
   char usuario [25];
   char password [120];
+  int typeUser;
+  int logged;
+  struct Usuario* siguiente;
 };
 
 struct Categoria{
   int ID_categoria;
   char descripcion [25];
+  struct Categoria* siguiente;
 };
 
 
@@ -21,6 +31,7 @@ struct Producto {
   char descripcion [100]; 
   long double precio;
   int ID_categoria;
+  struct Producto* siguiente;
 };
 
 struct Venta {
@@ -30,6 +41,7 @@ struct Venta {
   long double isr;
   long double monto_total;
   int ID_usuario;
+  struct Venta* siguiente;
 };
 
 struct DetalleVenta {
@@ -38,24 +50,42 @@ struct DetalleVenta {
   long double precio_unitario;
   int descuento;//por definir
   int cantidad;
+  struct DetalleVenta* siguiente;
 };
 
 struct Proveedor{
  int ID_proveedor;
  char descripcion [150];
+  struct Proveedor* siguiente;
 };
 
-struct adquisicion{
+struct Adquisicion{
   int ID_provedor;
   int ID_producto;
   int ID_usuario;
   char fecha [19]; //HH:mm:ss-XX/XX/XXXX
   int cantidad;
+  struct Adquisicion* siguiente;
 };
 
+void signin(char usuario,char password);
+void signout();
 
 
 
+/**********Control de datos***********/
+struct Producto* newProducto(int ID_producto,const char* nombre,int cantidad,const char* descripcion, long double precio,int ID_categoria);
+void insertarProducto(struct Producto** InicioLista, int ID_producto,const char* nombre,int cantidad,const char* descripcion, long double precio,int ID_categoria);
+void freeProducto(); //por hacer
+/***********************************/
+
+
+
+
+/*************Queries***************/
+void selectUsers(struct Usuario usuario);
+void selectProducts();
+/***********************************/
 
 
 #endif
