5aa975bed6204fd24d96c33f1b60adeaa0741bdb
Marlon <marlonrodriguez2b@gmail.com>
2023-11-23 16:46:23 -0600

    ci:working before refactor

--- proyecto/server.c
+++ proyecto/server.c
@@ -5,12 +5,15 @@ int main(){
   int resultado  = system(comando);
   if(resultado== -1) ErrorMessage("No se pudo limpiar la MC y semaforos");
   //Creamos hilos
-  pthread_t id_thread;
+  pthread_t id_thread1,id_thread2;
   int threadCorrect;
   pthread_attr_t atributos;
   pthread_attr_init(&atributos);
   pthread_attr_setdetachstate(&atributos,PTHREAD_CREATE_DETACHED);
-  threadCorrect = pthread_create(&id_thread,&atributos,H_login,NULL/*(void *) 0*/);
+  threadCorrect = pthread_create(&id_thread1,&atributos,H_login,NULL/*(void *) 0*/); //se crea hilo
+  if(threadCorrect !=0) ErrorMessage("\nError al crear hilo\n");
+  //otro hilo
+  threadCorrect = pthread_create(&id_thread2,&atributos,H_addItem,NULL/*(void *) 0*/); //se crea hilo
   if(threadCorrect !=0) ErrorMessage("\nError al crear hilo\n");
   while (1){
     /* code */
@@ -37,9 +40,9 @@ void up(int semid){
   struct sembuf op_v[]={0,+1,0};
   semop(semid,op_v,1);
 };
-void initSemaphore(key_t *llave,int *semaforo,int *clave){
-  *llave = ftok("archivo",*clave);
-  *semaforo = Crea_semaforo(*llave,1);
+void createSemPublic(int *semaforo,int clave, int valor){
+  key_t llave = ftok("archivo",clave);
+  *semaforo = Crea_semaforo(llave,valor);
 };
 int getValueSemaphore(int *id){
   return semctl(*id,0,GETVAL,0);
@@ -55,8 +58,8 @@ void *H_login(){
   int semaforo__h__login, semaforo_h_login_Req;
   int clave = 12; //Cambiar clave
   int count =0;
-  initSemaphore(&llave_serv_H_login,&semaforo__h__login,&clave);
-  initSemaphore(&llave_serv_H_login_Req,&semaforo_h_login_Req,&clave);
+  //initSemaphore(&llave_serv_H_login,&semaforo__h__login,&clave);
+  //initSemaphore(&llave_serv_H_login_Req,&semaforo_h_login_Req,&clave);
   getShmLogin(&usuario,CLAVE_SER_H_LOGIN_MC);
   getShmRequest(&count,CLAVE_SER_H_LOGIN_MC_REQ);
   printf("Servicios activados, servicio login escuchando...\n");
@@ -93,7 +96,16 @@ void *H_getItem(int dataType){
 };
 void *Dispatch_H_getItem(){};
 
-void H_addItem(void *data,int dataType){
+void *H_addItem(void *data,int dataType){
+
+  /**
+   * No recibe argumentos
+   * crea hilos que van a atender
+   * pide memoria compartida donde observa si llego algun request
+   * Crea el semaforo para avisar si esta ocupando un hiloDispatcher
+   * este hilo esta pendiente de algun request de tipo addItem
+   * si llega lgun request cre al hilo para atenderlo 
+  */
   struct FileManager fileManager;
   char salida [200];
   fileManager.fileName[0]='\0';
@@ -179,14 +191,33 @@ void *H_readFile(){};
 /****************************************************************/
 /*************** Controller Endpoints Request *******************/
 void *dispatch_H_addItem(pid_t ID_client, int dataType){
+  int semaforo_mutex,semaforo_LE;
   switch (dataType){
   case USUARIO:
-    //Pedimos la memoria compartida para usuario
     struct Usuario usuario;
-    key_t llave_H_addItem_usuario = ftok("archivo",ID_client);
-    int memoria = shmget(llave_H_addItem_usuario, sizeof(struct Usuario), IPC_CREAT | PERMISOS);
-    if(memoria == -1) ErrorMessage("Error al pedir region de memoria compartida en usuario");
-    usuario.apt_mc_usuario = (struct Usuario*) shmat(memoria,0,0);
+    //Pedimos la memoria compartida para usuario
+    getShmPublic(&usuario,ID_client,USUARIO);
+    //hacemos semaforo
+    createSemPublic(&semaforo_mutex,ID_client,0);
+    createSemPublic(&semaforo_LE,ID_client,1);
+    //mientras las queries sean mayor de 0
+    while (usuario.apt_mc_usuario->count > 0){
+      if(getValueSemaphore(&semaforo_mutex) == 0){ // Termino de escribir el cliente
+        //semafo_LE down
+        down(semaforo_LE);
+        //Copia datos de la estructura
+        /*********/
+        strcpy(usuario.nombre,usuario.apt_mc_usuario->nombre);
+        strcpy(usuario.password,usuario.apt_mc_usuario->password);
+        usuario.typeUser =usuario.apt_mc_usuario->typeUser;
+        /*********/
+        //semaforo_LE up
+        up(semaforo_LE);
+        //Le hace saber al clienteque ya copiamos los datos
+        up(semaforo_mutex);
+      };      
+    };
+    
     break;
   
   default:
@@ -201,13 +232,13 @@ void  dispatch_H_login(struct Usuario *usuario){
   char *username = usuario->apt_mc_usuario->usuario;
   char *password = usuario->apt_mc_usuario->password;
   openFile(&fileManager);
-  searchInDoc(&fileManager,usuario,usuario,usuario);
+  //searchInDoc(&fileManager,usuario,usuario,usuario);
   closeFile(&fileManager);
 
   return;
 };
 //Busca en el archivo y rellena la estructura en caso de que si exista alguna coincidencia
-void searchInDoc(struct FileManager *file,void *data, void *field, void *value){
+void searchInDoc(struct FileManager *file){
   //Buscar dato en en 
   return;
 };
@@ -215,6 +246,31 @@ void searchInDoc(struct FileManager *file,void *data, void *field, void *value){
 /****************************************************************/
 
 /******************** Memoria Compartida ************************/
+void getShmPublic(void *data,char clave,int typeData){ //Falta terminar
+  key_t llave_memoria = ftok("archivo",clave);
+  int memoria;
+  switch (typeData){
+  case USUARIO:
+    struct Usuario *usuario = (struct Usuario*)data;
+    memoria = shmget(llave_memoria, sizeof(struct Usuario), IPC_CREAT | PERMISOS);
+    if(memoria == -1) ErrorMessage("\nError al pedir region en la memoria compartida de getShmPublic/USUARIO");
+    usuario->apt_mc_usuario = (struct Usuario*)shmat(memoria,0,0);
+    if(usuario->apt_mc_usuario == (struct Usuario*)(-1)) ErrorMessage("\nError al signar la memoria compartida al apuntador de getShmPublic/USUARIO");
+    break;
+  case CATEGORIA:
+    struct Categoria *categoria = (struct Categoria*)data;
+    memoria = shmget(llave_memoria, sizeof(struct Categoria), IPC_CREAT | PERMISOS);
+    if(memoria == -1) ErrorMessage("\nError al pedir region en la memoria compartida de getShmPublic/CATEGORIA");
+    categoria->apt_mc_categoria = (struct Categoria*)shmat(memoria,0,0);
+    if(categoria->apt_mc_categoria == (struct Categoria*)(-1)) ErrorMessage("\nError al signar la memoria compartida al apuntador de getShmPublic/CATEGORIA");
+    break;
+  //Falta terminar todos las tablas
+    default:
+    break;
+  };
+  return;
+}
+
 void getShmRequest(int *count, char clave){
   key_t llave_memoria = ftok("archivo", clave);
   int memoria  = shmget(llave_memoria,sizeof(int), IPC_CREAT | PERMISOS);
