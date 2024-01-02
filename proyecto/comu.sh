8edf812dc510cb90f67a42375a574f9ac8932309
Marlon <marlonrodriguez2b@gmail.com>
2024-01-01 20:37:40 -0600

    bash program to compile user app

---
+++ proyecto/comu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#chmod +x compilar.sh //Para poder utilizarlo en terminal
+echo -e "Compilando el programa....\n\n"
+nombre="main"
+
+gcc "$nombre.c" -o "$nombre" controller.c model.c view.c -lncurses -lmenu -lform
+
+if [ $? -eq 0 ]; then
+  echo  "programa compilado!"
+  #./$nombre
+else
+  echo -e "\n\nError en la compilacion!"
+fi
